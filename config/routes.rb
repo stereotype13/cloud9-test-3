f3b627857881126eeda39f58ba24f24def94445a
OpenShift guest <robert.hodel@ladwp.com>
2013-12-30 13:47:55 -0500

    Create welcome controller with index and about views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Cloud9Test3::Application.routes.draw do
+  get "welcome/index"
+
+  get "welcome/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
